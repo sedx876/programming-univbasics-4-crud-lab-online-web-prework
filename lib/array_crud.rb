17cbce08a471f555c4f13facfebc1b39deed5b34
Sharon Watkins <swatkins876@gmail.com>
2019-08-04 13:39:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,11 +27,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array[index_number]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  retrieve_last_element_from_array[-1]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
